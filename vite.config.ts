c80f26732fff1deb5eccd4d274bbab7b5a41927a
kuccii <ypattos@gmail.com>
2025-06-24 16:25:18 +0200

    feat: add sitemap and robots.txt for SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/robots.txt',
+          dest: ''
+        },
+        {
+          src: 'public/sitemap.xml',
+          dest: ''
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
