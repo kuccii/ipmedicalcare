ebdf7e932945a36103bf77a3acae988725d650a0
kuccii <ypattos@gmail.com>
2025-07-21 15:34:34 +0200

    fix: remove vite-plugin-static-copy and use postbuild script for static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/robots.txt',
-          dest: ''
-        },
-        {
-          src: 'public/sitemap.xml',
-          dest: ''
-        }
-      ]
-    })
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
